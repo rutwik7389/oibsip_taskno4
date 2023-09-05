b91850df2d29ab0cd85225d83c19c966335a0816
Rutwik sindkar <140788000+rutwik7389@users.noreply.github.com>
2023-09-05 20:54:30 +0530

    Add files via upload

---
+++ newpage.java
@@ -0,0 +1,16 @@
+//import required classes and packages  
+import javax.swing.*;  
+import java.awt.*;  
+  
+//create NewPage class to create a new page on which user will navigate  
+class newpage extends JFrame  
+{  
+    //constructor  
+    newpage()  
+    {  
+        setDefaultCloseOperation(javax.swing.  
+        WindowConstants.DISPOSE_ON_CLOSE);  
+        setTitle("Welcome");  
+        setSize(400, 200);  
+    }  
+}  
